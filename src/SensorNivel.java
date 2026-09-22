cfaabc092ff6008e8f42005deb554fd1630fd3cc
range <frangel27@alumnos.uaq.mx>
2026-09-21 22:43:26 -0600

    Agrega lectura y validación del sensor

--- src/SensorNivel.java
+++ src/SensorNivel.java
@@ -1,10 +1,26 @@
 public class SensorNivel {
-    private final String id;
+    private String id;
+    private double valor_medio;
+    private double lim_minimo;
+    private double lim_maximo;
 
-    public SensorNivel(string id){
-         this.id = id;
+    public SensorNivel(String id,  double valor_medio, double lim_minimo, double lim_maximo) {
+        this.id = id;
+        this.valor_medio = 0;
+        this.lim_minimo = lim_minimo;
+        this.lim_maximo = lim_maximo;
     }
-    public String getId(){
-        return this.id;
- }
+
+    public String getId() {
+        return id;
+    }
+    public void  lectura_sensor(double nivel_tanque){
+        this.valor_medio = nivel_tanque;
+    }
+    public double getValor_medio() {
+        return valor_medio;
+    }
+    punlic boolean lec_valida(){
+        return valor_medio >= lim_minimo && valor_medio <= lim_maximo;
+    }
 }
